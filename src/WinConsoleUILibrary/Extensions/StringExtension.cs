c0d530af8a71f008a527c77af5d6415461f1286b
Christian Popescu <christian.popescu@outlook.com>
2020-06-11 00:14:49 +0200

    Add some usefull classes

---
+++ src/WinConsoleUILibrary/Extensions/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WinConsoleUILibrary.Misc
+{
+    public static class StringExtension 
+    {
+        public static string SafeSubstring(this String str, int Index, int Length) 
+        {
+            if (str.Length-1 < Index) return "";
+            return str.Substring(Index, Math.Min(str.Length-Index, Length));
+
+        }
+    }
+}
---
+++ src/WinConsoleUILibrary/Tools/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SanboxConsole
+{
+    public class Position
+    {
+
+        public Position():this(0,0) {}
+        public Position(int r, int c)
+        { 
+            Raw = r;
+            Column = c;
+        }
+        public int Raw {get; set;}
+        public int Column {get; set;}
+    }
+}
