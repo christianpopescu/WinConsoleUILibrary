07e34740ffb0a17c0f18b1f6d4962a137fd1ec69
Christian Popescu <christian.popescu@outlook.com>
2020-06-12 23:49:56 +0200

    InputField added methods

--- src/WinConsoleUILibrary/Controls/InputField.cs
+++ src/WinConsoleUILibrary/Controls/InputField.cs
@@ -2,6 +2,27 @@ namespace WinConsoleUILibrary.Controls
 {
     public class InputField
     {
-        
+        int MaxSize {get; set;}
+        string Value {get; set;}
+        int CursorPosition {get; set;}
+
+        private InputField()
+        {
+            CursorPosition = 0;
+            Value = "";
+        }
+
+        public InputField(int maxSize) : this() => MaxSize = maxSize;
+
+        public void AddChar(char chr) 
+        {
+            if (CursorPosition < MaxSize + 1) 
+            {
+                Value += chr;
+                CursorPosition++;
+            }
+
+        }
+
     }
 }
