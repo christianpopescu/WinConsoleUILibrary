b8ee364ee47ac769ffb149206a91e49a9d3483f3
Christian Popescu <christian.popescu@outlook.com>
2020-06-16 23:57:57 +0200

    Size Added

---
+++ src/WinConsoleUILibrary/Tools/Size.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace WinConsoleUILibrary.Tools
+{
+    public class Size : Coordinate2D
+    {
+
+        public Size():base(0,0) {}
+        
+        public Size(int r, int c) : base(r,c) {}
+
+    }
+}
