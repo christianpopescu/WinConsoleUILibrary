c65dfad53c0f8c711952c86667f9ab32f83697f9
Christian Popescu <christian.popescu@outlook.com>
2020-06-17 00:02:47 +0200

    Add some methods to the window class

--- src/WinConsoleUILibrary/Controls/Window.cs
+++ src/WinConsoleUILibrary/Controls/Window.cs
@@ -1,7 +1,12 @@
+using WinConsoleUILibrary.Tools;
+
 namespace WinConsoleUILibrary.Controls
 {
     public class Window
     {
         
+        public Position TopLeft;
+
+        public CSize Size;
     }
 }
---
+++ src/WinConsoleUILibrary/Tools/CSize.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace WinConsoleUILibrary.Tools
+{
+    public class CSize : Coordinate2D
+    {
+
+        public CSize():base(0,0) {}
+        
+        public CSize(int r, int c) : base(r,c) {}
+
+    }
+}
