0624c05432e95a50dee8eb2cb58afa396b9d727c
Christian Popescu <christian.popescu@outlook.com>
2020-07-05 00:50:04 +0200

    Add simple console application  + code clean

---
+++ src/WinConsoleUILibrary/Application/IApplication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinConsoleUILibrary.Application
+{
+    public interface IApplication
+    {
+        void Run();
+    }
+}
---
+++ src/WinConsoleUILibrary/Application/SimpleConsoleApplication.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WinConsoleUILibrary.Controls;
+
+namespace WinConsoleUILibrary.Application
+{
+    /// <summary>
+    /// Simple Console Class
+    ///    - contains only one window
+    /// </summary>
+    public class SimpleConsoleApplication : IApplication
+    {
+        protected Window theWindow = new Window();
+        public void Run()
+        {
+            theWindow.Run();
+        }
+
+        public SimpleConsoleApplication AddControlToWindow(IControl control)
+        {
+            theWindow.AddControl(control);
+            return this;
+
+        }
+    }
+}
